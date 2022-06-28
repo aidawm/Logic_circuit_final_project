b83138977879126d96ace9ec327090388fc133ad
aida <aidaw.mobli@gmail.com>
2022-06-28 12:03:19 +0430

    module 5 structure is added

---
+++ module_5/controller_and_register.v
@@ -0,0 +1,25 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    11:58:42 06/28/2022 
+// Design Name: 
+// Module Name:    controller_and_register 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module controller_and_register(
+    );
+
+
+endmodule
